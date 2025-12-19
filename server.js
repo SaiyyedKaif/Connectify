b0388a36de1acb7ea1afaf8e64e31c63b7a7d34e
SaiyyedKaif <146384933+SaiyyedKaif@users.noreply.github.com>
2025-12-19 20:53:09 +0530

    Update Redis client connection URL

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const botName = "Connectify Bot";
 
 (async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+  pubClient = createClient({ url: "redis://default:oxOIaopHngBrseavgtNGfCPLtiDSStON@redis.railway.internal:6379" });
   await pubClient.connect();
   subClient = pubClient.duplicate();
   io.adapter(createAdapter(pubClient, subClient));
